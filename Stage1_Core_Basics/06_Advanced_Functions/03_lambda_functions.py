7fb6a1e933eb79bdb3a4cf63f7f2d4a660bc2417
fanbyteio <gisfanyunfei@gmail.com>
2026-03-15 20:26:51 +0800

    feat: init repo and upload Stage 1 basic scripts

--- Stage1_Core_Basics/06_Advanced_Functions/03_lambda_functions.py
+++ Stage1_Core_Basics/06_Advanced_Functions/03_lambda_functions.py
@@ -1,3 +1,21 @@
+# 函数作为参数传递
+def test_func(func):
+    result = func(1, 2)
+    print(f"func参数的类型是”：{func}")
+    print(f"计算结果：{result}")
+
+
+def compute(x, y):
+    return x + y
+
+test_func(compute)
 
 
 
+
+# lambda匿名函数
+def test_func(func):
+    result = func(1, 2)
+    print(f"计算结果：{result}")
+
+test_func(lambda x, y : x + y)
