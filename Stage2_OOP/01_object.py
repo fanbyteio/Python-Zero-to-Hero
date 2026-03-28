7f5104957894832479a41e092fd370a8e2290ae2
fanbyteio <gisfanyunfei@gmail.com>
2026-03-28 16:00:43 +0800

    feat:对象

---
+++ Stage2_OOP/01_object.py
@@ -0,0 +1,24 @@
+# 设计一个类
+class student:
+    name = None
+    gender = None
+    nationality = None
+    native_place = None
+    age = None
+
+# 创建一个对象
+stu_1 = student()
+
+# 对象属性负值
+stu_1.name = "林俊杰"
+stu_1.gender = "男"
+stu_1.nationality = "中国"
+stu_1.native_place = "上海市"
+stu_1.age = 20
+
+# 获取对象中记录的信息
+print(stu_1.name)
+print(stu_1.gender)
+print(stu_1.nationality)
+print(stu_1.native_place)
+print(stu_1.age)
