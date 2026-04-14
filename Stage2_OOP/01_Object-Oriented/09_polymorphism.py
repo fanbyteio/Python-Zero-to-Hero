22d80acbe5f173e8f3c61b8d29063dc6f8b9b4d4
fanbyteio <gisfanyunfei@gmail.com>
2026-04-14 12:07:18 +0800

    feat:

---
+++ Stage2_OOP/01_Object-Oriented/09_polymorphism.py
@@ -0,0 +1,20 @@
+class Animal:
+    def speak(self):
+        pass
+
+class Dog(Animal):
+    def speak(self):
+        print("汪汪汪")
+
+class Cat(Animal):
+    def speak(self):
+        print("喵喵喵")
+
+def make_noise(animal:Animal):
+    animal.speak()
+
+dog = Dog()
+cat = Cat()
+
+make_noise(dog)
+make_noise(cat)
